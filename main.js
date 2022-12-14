b0fde51538e583642c64cf770728b9b61343682f
ZzCristianzZ <crisfegomez@gmail.com>
2022-12-14 18:36:19 -0500

    Mi primer Commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const h1 = document.querySelector('h1');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const pResult = document.querySelector('#result');
+const form = document.querySelector('#form')
+
+
+/*btn.addEventListener('click', btnOnclick);
+
+
+ function btnOnclick(){
+        const sumaInputs = input1.value + input2.value;
+     pResult.innerText = "Resultado : " + sumaInputs;
+    }
+*/
+
+form.addEventListener('submit', sumarInputValues);
+
+function sumarInputValues(event){
+    event.preventDefault();
+    const sumaInputs = input1.value + input2.value;
+    pResult.innerText = "Resultado: " + sumaInputs;
+}
+
+
+
+
+
